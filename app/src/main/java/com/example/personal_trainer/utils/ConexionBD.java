f655c8c48325d483bcc120cb49bc51135cd1a8dd
Miguel Angel Machuca Yavita <167807941+Miguel-Machuca@users.noreply.github.com>
2025-03-18 08:52:56 -0400

    feat: actualizar la estructura de la base de datos y tablas iniciales

--- app/src/main/java/com/example/personal_trainer/utils/ConexionBD.java
+++ app/src/main/java/com/example/personal_trainer/utils/ConexionBD.java
@@ -16,6 +16,8 @@ public class ConexionBD extends SQLiteOpenHelper {
     public static final String TABLE_OBJETIVO = "objetivo";
     public static final String TABLE_DETALLECLIENTE = "detalle_cliente";
     public static final String COLUMN_ID = "id";
+    public static final String FK_ID_CLIENTE = "id_cliente";
+    public static final String FK_ID_OBJETIVO = "id_objetivo";
     public static final String COLUMN_NOMBRE = "nombre";
     public static final String COLUMN_URLIMAGEN = "url_imagen";
     public static ConexionBD instance;
@@ -36,6 +38,11 @@ public class ConexionBD extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(getCreateTableEjercicio());
         sqLiteDatabase.execSQL(getCreateTableObjetivo());
+        sqLiteDatabase.execSQL(getCreateTableCliente());
+        sqLiteDatabase.execSQL(getCreateTableDetalleCliente());
+        sqLiteDatabase.execSQL(getCreateTableRutina());
+        sqLiteDatabase.execSQL(getCreateTableDetalleRutina());
+        sqLiteDatabase.execSQL(getCreateTableCronograma());
     }
 
     @Override
@@ -54,7 +61,6 @@ public class ConexionBD extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_DETALLECLIENTE);
     }
 
-
     private String getCreateTableEjercicio(){
         return "CREATE TABLE IF NOT EXISTS " + TABLE_EJERCICIO + "(" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -68,10 +74,51 @@ public class ConexionBD extends SQLiteOpenHelper {
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_NOMBRE + " TEXT NOT NULL);";
     }
-    private String getCreateTableRutina(){
-        return "CREATE TABLE IF NOT EXISTS " + TABLE_RUTINA + " (" +
+
+    private String getCreateTableCliente() {
+        return "CREATE TABLE IF NOT EXISTS " + TABLE_CLIENTE + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NOMBRE + " TEXT NOT NULL, " +
+                COLUMN_APELLIDO + " TEXT NOT NULL, " +
+                COLUMN_CELULAR + " TEXT NOT NULL);";
+    }
+
+    private String getCreateTableDetalleCliente() {
+        return "CREATE TABLE IF NOT EXISTS " + TABLE_DETALLECLIENTE + "(" +
+                FK_ID_CLIENTE + " INTEGER NOT NULL, " +
+                FK_ID_OBJETIVO + " INTEGER NOT NULL, " +
+                COLUMN_DESCRIPCION + " TEXT, " +
+                "PRIMARY KEY (" + FK_ID_CLIENTE + ", " + FK_OBJETIVO + "), " +
+                "FOREIGN KEY (" + FK_ID_CLIENTE + ") REFERENCES " + TABLE_CLIENTE + "(" + COLUMN_ID + "), " +
+                "FOREIGN KEY (" + FK_ID_OBJETIVO + ") REFERENCES " + TABLE_OBJETIVO + "(" + COLUMN_ID + "));";
+    }
+
+    private String getCreateTableRutina() {
+        return "CREATE TABLE IF NOT EXISTS " + TABLE_RUTINA + "(" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_NOMBRE + " TEXT NOT NULL);";
+    }
+
+    private String getCreateTableDetalleRutina() {
+        return "CREATE TABLE IF NOT EXISTS " + TABLE_DETALLERUTINA + " (" +
+                FK_ID_RUTINA + " INTEGER NOT NULL, " +
+                FK_ID_EJERCICIO + " INTEGER NOT NULL, " +
+                COLUMN_CANTIDADSERIE + " INTEGER NOT NULL, " +
+                COLUMN_CANTIDADREPETICION + " INTEGER NOT NULL, " +
+                COLUMN_DURACIONREPOSO + " INTEGER NOT NULL, " +
+                "PRIMARY KEY (" + FK_ID_RUTINA + ", " + FK_ID_EJERCICIO + "), " +
+                "FOREIGN KEY (" + FK_ID_RUTINA + ") REFERENCES " + TABLE_RUTINA + "(" + COLUMN_ID + "), " +
+                "FOREIGN KEY (" + FK_ID_EJERCICIO + ") REFERENCES " + TABLE_EJERCICIO + "(" + COLUMN_ID + "));";
+    }
+
+    private String getCreateTableCronograma() {
+        return "CREATE TABLE IF NOT EXISTS " + TABLE_CRONOGRAMA + "(" +
+                COLUMN_ID_CRONOGRAMA + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_FECHA + " DATE, " +
+                FK_ID_CLIENTE + " INTEGER NOT NULL, " +
+                FK_ID_RUTINA + " INTEGER NOT NULL, " +
+                "FOREIGN KEY (" + FK_ID_CLIENTE + ") REFERENCES " + TABLE_CLIENTE + "(" + COLUMN_ID + "), " +
+                "FOREIGN KEY (" + FK_ID_RUTINA + ") REFERENCES " + TABLE_RUTINA + "(" + COLUMN_ID + "));";
     }
 
 }
